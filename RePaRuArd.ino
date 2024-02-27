8310a6daa368a2af4298dd233e20980a5f4344dd
Amir_smd1337 <109676906+Amir1202@users.noreply.github.com>
2024-02-27 23:51:56 +0400

    Add files via upload
    add accelerometr

--- RePaRuArd.ino
+++ RePaRuArd.ino
@@ -1,8 +1,17 @@
 #include "Parser.h"                    //Библиотека Гайвера для парсинга данных
 #include "AsyncStream.h"               // асинхронное чтение сериал
-AsyncStream<50> serial(&Serial, ';');  // указываем обработчик и стоп символ
+AsyncStream<50> serial(&Serial, ';');  // Создаём объект и указываем обработчик и стоп символ
 
-//Библиотека для датчика температуры - используется "DIGITAL" пин 34
+
+
+#include <TroykaIMU.h>        // Библиотека для IMU - датчика
+Gyroscope gyroscope;          // Создаём объект для работы с гироскопом
+Accelerometer accelerometer;  // Создаём объект для работы с акселерометром
+Compass compass;              // Создаём объект для работы с магнитометром/компасом
+Barometer barometer;          // Создаём объект для работы с барометром
+
+
+//Библиотека для датчика температуры
 #include "microDS18B20.h"
 uint8_t addrTempBig[] = { 0x28, 0xFF, 0x64, 0x1E, 0xF7, 0x9E, 0xF4, 0x96 };
 uint8_t addrTempLittle[] = { 0x28, 0x72, 0x8, 0x48, 0xF6, 0xF5, 0x3C, 0xD };
@@ -13,12 +22,17 @@ MicroDS18B20<A1, addrTempLittle> dsLittle;
 #include "Servo.h"
 Servo ESC;
 Servo SER;
-#define ESC_pin 3
-#define SER_pin 5
+#define ESC_pin 3                   // = 3 = шим к ESC
+#define SER_pin 5                   // = 5 = шим к Servo
 
 void setup() {
   Serial.begin(115200);
   pinMode(13, 1);
+  Serial.println("IMU Begin");      // Выводим сообщение о начале инициализации
+  gyroscope.begin();                // Инициализируем гироскоп
+  accelerometer.begin();            // Инициализируем акселерометр
+  compass.begin();                  // Инициализируем компас
+  barometer.begin();                // Инициализируем барометр
   ESC.attach(ESC_pin);
   ESC.writeMicroseconds(1600);
   delay(3000);
@@ -28,9 +42,10 @@ void setup() {
 }
 void loop() {
   parsing();
-  Datch();
+  Sensor_0();
+  Accelerometer_1();
 }
-void Datch() {
+void Sensor_0() {
   static uint32_t tmr = 0;
   if (millis() - tmr > 800) {
     tmr = millis();
@@ -42,12 +57,23 @@ void Datch() {
     Serial.print(',');
     dsLittle.requestTemp();  //ЗапросТемпературы2.
     dsLittle.readTemp();     //Читаем, если прочитано - выводим.
-    Serial.print(dsLittle.getTemp());
-
-    Serial.print(',');
-    Serial.println("1000");
+    Serial.println(dsLittle.getTemp());
   }
 }
+void Accelerometer_1() {
+  static uint32_t tmr = 0;
+  if (millis() - tmr > 100) {
+    tmr = millis();
+    Serial.print(1);
+    Serial.print(',');
+    Serial.print(accelerometer.readAccelerationAX());
+    Serial.print(',');
+    Serial.print(accelerometer.readAccelerationAY());
+    Serial.print(',');
+    Serial.println(accelerometer.readAccelerationAZ());
+  }
+}
+
 // функция парсинга
 void parsing() {
   if (serial.available()) {
@@ -65,13 +91,13 @@ void parsing() {
         SER.write(ints[1]);
         break;
       case 3:
-        ESC.write(1600);            //Максимум вперёд!
+        ESC.write(2300);  //Максимум вперёд!
         break;
       case 4:
-        ESC.write(1450);            //СТОП!
+        ESC.write(1465);  //СТОП!
         break;
       case 5:
-        ESC.write(1360);            //Максимум назад!
+        ESC.write(800);  //Максимум назад!
         break;
     }
   }
